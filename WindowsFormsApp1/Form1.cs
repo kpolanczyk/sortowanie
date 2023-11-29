0848fc9d32adc40a29cb3cf83f32a60eb63cfc8c
kpolanczyk <kacper.pol2002@gmail.com>
2023-11-29 18:57:47 +0100

    aktualilzacja 1

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -1,7 +1,10 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlTypes;
+using System.Diagnostics.Eventing.Reader;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -31,8 +34,8 @@ namespace WindowsFormsApp1
 
             for (int i = 0; i < 10; i++)
             {
-                this.array[i] = rnd.Next(10);
-                Generowane.Items.Add(rnd.Next(this.array[i]));
+                this.array[i] = rnd.Next(100);
+                Generowane.Items.Add((this.array[i]));
             }
         }
 
@@ -67,8 +70,74 @@ namespace WindowsFormsApp1
                 }
             }
         }
+        private void Selectionsort(List<int> arr)
+        {
+            int n = arr.Count;
 
+            for (int i = 0; i < n - 1; i++)
+            {
+                int minIndex = i;
+                for (int j = i ; j < n; j++)
+                {
+                    if (arr[j] < arr[minIndex])
+                    {
+                        minIndex = j;
+                    }
+                }
+                int temp = arr[minIndex];
+                arr[minIndex] = arr[i];
+                arr[i] = temp;
+            }
+        }
         
+
+        private List<int> Insertionsort(List<int> arr)
+        {
+            int n = arr.Count;
+            for (int i = 0; i < n; i++)
+            {
+                int key = arr[i];
+                int j = i - 1;
+
+                while(j >= 0 && arr[j] > key)
+                {
+                    arr[j + 1] = arr[j];
+                    j = j - 1;
+                }
+                arr[j + 1] = key;
+            }
+            return arr;
+        } 
+
+        private void Quicksort(List<int> arr, int left, int right)
+        {
+            if (left<right)
+            {
+                int pi = Partition(arr, left, right);
+                Quicksort(arr, left, pi - 1);
+                Quicksort(arr, pi + 1, right);
+            }
+        }
+        private int Partition(List<int> arr, int left, int right)
+        {
+            int pivot = arr[right];
+            int i = left -1;
+
+            for(int j = left; j < right; j++)
+            {
+                if (arr[j] < pivot)
+                {
+                    i++;
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+            int temp1 = arr[i + 1];
+            arr[i + 1] = arr[right];
+            arr[right] = temp1;
+            return i + 1;
+        }
         private void Wybor_CheckedChanged(object sender, EventArgs e)
         {
 
@@ -95,7 +164,20 @@ namespace WindowsFormsApp1
             {
                 Bubblesort(DoPosortowania);
             }
-            
+            else if (Wybor.Checked)
+            {
+                Selectionsort(DoPosortowania);
+            }
+            else if (Wstawianie.Checked)
+            {
+                Insertionsort(DoPosortowania);
+            }
+            else if (Szybkie.Checked)
+            {
+                Quicksort(DoPosortowania, 0, DoPosortowania.Count -1);
+            }
+
+            Sortowane.Items.Clear();
 
             foreach (var item in DoPosortowania)
             {
